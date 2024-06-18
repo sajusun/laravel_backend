978b4b3baf56b637a91368186862b770aa375aa3
sajusun <sajuislam266@gmail.com>
2024-06-18 15:17:04 +0600

    add css

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,31 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+Route::get('services', function () {
+    return view('services');
+});
+Route::get('portfolio', function () {
+    return view('portfolio');
+});
+
+Route::get('elements', function () {
+    return view('elements');
+});
+Route::get('portfolio-details', function () {
+    return view('portfolio-details');
+});
+Route::get('blog', function () {
+    return view('blog');
+});
+Route::get('single-blog', function () {
+    return view('single-blog');
+});
+Route::get('contact', function () {
+    return view('contact');
 });

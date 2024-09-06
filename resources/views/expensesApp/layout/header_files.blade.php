272d1388c26ccb732437fd18c2314295c4d4084f
sajusun <sajuislam266@gmail.com>
2024-09-07 03:20:45 +0600

    add filter search

--- resources/views/expensesApp/layout/header_files.blade.php
+++ resources/views/expensesApp/layout/header_files.blade.php
@@ -4,11 +4,13 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script src="{{asset('/js/classes.js')}}"></script>
 <script src="{{asset('/js/global-var.js')}}"></script>
 
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 <link rel="stylesheet" href="{{asset('/css/bootstrap.css')}}">
 <link rel="stylesheet" href="{{asset('/css/font-awesome.min.css')}}">
 <link rel="stylesheet" href="{{asset('/css/custom-style.css')}}">
--- resources/views/expensesApp/pages/expenses_in.blade.php
+++ resources/views/expensesApp/pages/expenses_in.blade.php
@@ -12,18 +12,54 @@
 <div class="container main-section">
     <div class="row" style="padding-top: 15px">
         <div class="col" style="max-width: 180px">
-            <!-- Small button groups (default and split) -->
-            <div class="btn-group">
-                <button id="filterBtn" class="btn btn-secondary btn-sm dropdown-toggle" type="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Filter By Months
-                </button>
-                <div class="dropdown-menu" id="monthsName">
+            <h2 class="grid-title"><i class="fa fa-filter"></i> Filters</h2>
+            <hr>
+            <div class="filter-section">
+                <span>Filter By Month:</span>
+                {{--          Small button groups (default and split) --}}
+                <div class="btn-group">
+                    <button id="filterBtn" class="btn btn-info btn-sm dropdown-toggle" type="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Filter By Months
+                    </button>
+                    <div class="dropdown-menu" id="monthsName"></div>
+                    {{--                END filter by month --}}
                 </div>
-                <!-- END ORDER RESULT -->
+            </div>
+            <hr>
+            <div class="filter-section">
+                <span>Order By:</span>
+                <div class="btn-group">
+                    <button id="orderBtn" class="btn btn-info btn-sm dropdown-toggle" type="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Order By
+                    </button>
+                    <div class="dropdown-menu" id="ordersName">
+                        <a class="dropdown-item" href="#" id="date">Date</a>
+                        <a class="dropdown-item" href="#" id="amount">Amount</a>
+                        <a class="dropdown-item" href="#" id="descending">Ascending</a>
+                        <a class="dropdown-item" href="#" id="descending">Descending</a>
+                    </div>
+                    {{--                END filter by month --}}
+                </div>
+            </div>
+            <hr>
+            <div class="filter-section">
+                <input type="text" class="form-control" placeholder="Search">
             </div>
         </div>
+
         <div class="col">
+            <!-- BEGIN SEARCH INPUT -->
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search By Text | Date | Year like 2000">
+                <span class="input-group-btn">
+                <button class="btn btn-primary" type="button"><i class="fa fa-search"></i></button>
+              </span>
+            </div>
+            <!-- END SEARCH INPUT -->
+            <hr>
+
             <section id="data_table">
                 <table class="table table-bordered">
                     <thead>
@@ -62,17 +98,34 @@
         const date = new Date();
         let month = date.getMonth();
 
+
+        function detectMonth(x) {
+            if (month === parseInt(x)) {
+                return "This Month";
+            } else {
+                return monthsInArray[parseInt(x)];
+            }
+        }
+
         let monthNameDiv = $('#monthsName');
         let filterBtn = $('#filterBtn');
+
+        let orderNameDiv = $('#ordersName');
+        let orderBtn = $('#orderBtn');
+
         let elements = "";
         for (let i = 0; i < monthsInArray.length; i++) {
-            elements += `<a class="dropdown-item" href="#" id="${i}">${monthsInArray[i]}</a>`
+            elements += `<a class="dropdown-item" href="#" id="${i}">${detectMonth(i)}</a>`
         }
         monthNameDiv.html(elements);
-        filterBtn.text(monthsInArray[month])
+        filterBtn.text(detectMonth(month));
 
         monthNameDiv.click(function (event) {
-            filterBtn.text(event.target.text)
+            filterBtn.text(event.target.text);
+        });
+
+        orderNameDiv.click(function (event) {
+            orderBtn.text(event.target.text);
         });
 
         // fetch data from server
